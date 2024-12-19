96d741de09a6e8177d81081909ac25bf0a42159e
TargaMarcello <marcello.targa@iisviolamarchesini.edu.it>
2024-12-19 08:16:02 +0100

    modifiche

--- system-call/es1.c
+++ system-call/es1.c
@@ -3,8 +3,10 @@
 #include <fcntl.h>
 #include <unistd.h>
 
-int main(int argc, char* argv[]){
-    if(argc!=2){
+int main(int argc, char *argv[])
+{
+    if (argc != 2)
+    {
         printf("Errore numero argomenti!!!");
         exit(1);
     }
@@ -12,9 +14,9 @@ int main(int argc, char* argv[]){
     int fd, charRead;
     char buff;
     fd = open(argv[1], O_RDONLY);
-    while (charRead = read(fd, &buff,sizeof(buff))>0)
+    while (charRead = read(fd, &buff, sizeof(buff)) > 0)
     {
-        write(1,&buff,charRead);
+        write(1, &buff, charRead);
     }
     close(fd);
     return 0;
