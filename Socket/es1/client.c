90b5ee5b109fc66f9625afc19cec02af0c28177d
Marcello Targa <121225428+targamarcello@users.noreply.github.com>
2025-11-07 16:52:40 +0100

    Update client.c

--- Socket/es1/client.c
+++ Socket/es1/client.c
@@ -1,29 +1,28 @@
 //CLIENT
-#include <stdio.h>       //std in-out
-#include <stdlib.h>      //per utilizzo di certe funzioni:htonl,rand,....
-#include <sys/socket.h>  //funz. accept+bind+listen
-#include <sys/types.h>   //funz. accept
-#include <netinet/in.h>  //definiscono la struttura degli indirizzi 
-#include <string.h>      //funz. stringhe
-#include <errno.h>       //gestioni errori connessione
-#include <ctype.h>       //bind
-#include <unistd.h>     // file header che consente l'accesso alle API dello standard POSIX
+#include <stdio.h>
+#include <stdlib.h> 
+#include <sys/socket.h>
+#include <sys/types.h> 
+#include <netinet/in.h
+#include <string.h>  
+#include <errno.h>  
+#include <ctype.h>     
+#include <unistd.h>   
 
 #define DIM 50
 #define SERVERPORT 1313
 
 int main(int argc,char** argv)
-{   //creazione di un elemento di tipo sockaddr vedi pagina 162
+{   
     struct sockaddr_in servizio;
-     //inizializzazione dell'elemento di tipo sockaddr
+    
     servizio.sin_family=AF_INET;
-    servizio.sin_addr.s_addr=htonl(INADDR_ANY); //  La funzione htonl accetta un numero a 32 bit in ordine di byte host e restituisce un numero a 32 bit nell'ordine dei byte di rete usato nelle reti TCP/IP (la famiglia di indirizzi AF_INET o AF_INET6).
-    servizio.sin_port=htons(SERVERPORT);       //La funzione htons accetta un numero a 16 bit nell'ordine dei byte host e restituisce un numero a 16 bit nell'ordine dei byte di rete usato nelle reti TCP/IP (la famiglia di indirizzi AF_INET o AF_INET6).
-    char str1[DIM];     //Stringa da inviare
-    int socketfd;// identificatore della socket
-    //creazione e definizione del Socket di tipo stream tcp pg 163
+    servizio.sin_addr.s_addr=htonl(INADDR_ANY); 
+    servizio.sin_port=htons(SERVERPORT);       
+    char str1[DIM];     
+    int socketfd;
+    
     socketfd=socket(AF_INET,SOCK_STREAM,0);
-    //connessione al server pag.173
     connect(socketfd,(struct sockaddr*)&servizio,sizeof(servizio));    
     printf("Inserisci la stringa\n");
     scanf("%s",str1);
